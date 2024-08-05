50362aada3aa9222d7d248b335679ef8b0cf11e1
Viv696969 <chouhanvivek207@gmail.com>
2024-08-05 23:10:27 +0530

    analysis_2

--- Analysis/analysis.sql
+++ Analysis/analysis.sql
@@ -74,5 +74,39 @@ select * from cte_2_wheeler
 union
 select * from cte_4_wheeler;
 
+-- 3
+with cte_2022 as (
+select state,sum(electric_vehicles_sold)*100/sum(total_vehicles_sold) as `penetration rate`
+from electric_vehicle_sales_by_state ev join dim_date d using(`date`)
+where d.fiscal_year=2022
+group by state
+),
+cte_2024 as (
+select state,sum(electric_vehicles_sold)*100/sum(total_vehicles_sold) as `penetration rate`
+from electric_vehicle_sales_by_state ev join dim_date d using(`date`)
+where d.fiscal_year=2024
+group by state
+),
+cte_2023 as (
+select state,sum(electric_vehicles_sold)*100/sum(total_vehicles_sold) as `penetration rate`
+from electric_vehicle_sales_by_state ev join dim_date d using(`date`)
+where d.fiscal_year=2023
+group by state
+)
+select state,cte_2022.`penetration rate` as `penetration rate 2022`,
+cte_2023.`penetration rate` as `penetration rate 2023`,
+cte_2024.`penetration rate` as `penetration rate 2024`
+from cte_2022  join cte_2024 using (state) join cte_2023 using(state)
+-- where cte_2024.`penetration rate`> cte_2022.`penetration rate`
+order by `penetration rate 2024` desc
+limit 5
+;
+
+-- p rate by fiscal years
+select 
+fiscal_year,round(sum(electric_vehicles_sold)*100/sum(total_vehicles_sold),2) as `penetration rate`
+from electric_vehicle_sales_by_state ev join dim_date d using(`date`)
+group by fiscal_year
+;
 
 
